b6020d5a583f642dab2c50541e1558fc418ca762
aatreyeem <83920798+aatreyeem@users.noreply.github.com>
2022-07-14 10:06:08 +0530

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -1,20 +1,17 @@
-// C++ program to print DFS traversal from
-// a given vertex in a given graph
+
 #include <bits/stdc++.h>
 using namespace std;
 
-// Graph class represents a directed graph
-// using adjacency list representation
+
 class Graph {
 public:
 	map<int, bool> visited;
 	map<int, list<int> > adj;
 
-	// function to add an edge to graph
+	
 	void addEdge(int v, int w);
 
-	// DFS traversal of the vertices
-	// reachable from v
+	
 	void DFS(int v);
 };
 
@@ -25,23 +22,21 @@ void Graph::addEdge(int v, int w)
 
 void Graph::DFS(int v)
 {
-	// Mark the current node as visited and
-	// print it
+	
 	visited[v] = true;
 	cout << v << " ";
 
-	// Recur for all the vertices adjacent
-	// to this vertex
+	
 	list<int>::iterator i;
 	for (i = adj[v].begin(); i != adj[v].end(); ++i)
 		if (!visited[*i])
 			DFS(*i);
 }
 
-// Driver code
+e
 int main()
 {
-	// Create a graph given in the above diagram
+	
 	Graph g;
 	g.addEdge(0, 1);
 	g.addEdge(0, 2);
@@ -57,4 +52,4 @@ int main()
 	return 0;
 }
 
-// improved by Vishnudev C
+
